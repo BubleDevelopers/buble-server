7b5d0ca0c74cefcbb09f53b13920f291f1ce2cee
andrew <anfischl@gmail.com>
2015-06-28 20:13:10 -0400

    Removed literally all security

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x
 
 app.use(function(req, res, next) {
 	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+	res.header('Access-Control-Allow-Headers', '*');
 	res.header('Access-Control-Allow-Method', '*');
 	next();
 });
