4d19f02fbce39aa62b47669a4133f0066446ff79
Max Dumas <maltor124@gmail.com>
2015-07-01 00:03:01 -0400

    Update Checkin Model Schema

--- models/Checkin.js
+++ models/Checkin.js
@@ -2,12 +2,14 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var checkinSchema = new Schema({
-	userId: { type: Number, required: true },
+	userId: { type: Schema.ObjectId, required: true },
 	location: { 
 		lat: { type: Number, required: true },
 		long: { type: Number, required: true },
 		placeId: { type: String, required: true }
-	}
+	},
+	entered: { type: Date, required: true, default: Date.now() },
+	exited: { type: Date, required: false }
 });
 
 var Checkin = mongoose.model('Checkin', checkinSchema);
