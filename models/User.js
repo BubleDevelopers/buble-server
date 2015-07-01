a601912d0244820998c302d2db028dd5d1c98c5a
Max Dumas <maltor124@gmail.com>
2015-07-01 00:04:05 -0400

    Update User Model Schema

--- models/User.js
+++ models/User.js
@@ -3,10 +3,10 @@ var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
 	facebookId: { type: String, required: true },
-        googleId: { type: String, required: false },
+    googleId: { type: String, required: false },
 	twitterId: { type: String, required: false },
-	signupDate: { type: String, required: true, default: Date.now },
-        invisible: { type: Boolean, required: false, default: false }
+	signupDate: { type: Date, required: true, default: Date.now() },
+    invisible: { type: Boolean, required: false, default: false }
 });
 
 var User = mongoose.model('User', userSchema);
