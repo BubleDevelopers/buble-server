3f5c460082b1b69344c37f8c506c7d43f8700129
andrew <anfischl@gmail.com>
2015-07-07 16:35:53 -0400

    tried several things to get delete function of users route working but i can't get it... formatting in models/User was a little weird so i fixed it

--- routes/users.js
+++ routes/users.js
@@ -42,7 +42,7 @@ router.post('/', function(req, res) {
 	});
 });
 
-router.get('/:id', function(req, res, next) {
+router.get('/id', function(req, res, next) {
 	User.findById(req.params.id)
 		.then(function(user) {
 			res.status(200).json(user);
@@ -51,15 +51,69 @@ router.get('/:id', function(req, res, next) {
 		});
 });
 
-router.delete('/:id', function(req, res, next) {
+// NEW CODE
+router.delete('/id', function(req, res, next) {
+	res.send({
+		message: 'we are inside the delete user method'
+	});
+	console.log("Req.body:");
+	console.log(req.body);
+	User.remove({ _id: "ObjectId(" + req.body.id + ")" }, function(err) {
+		console.log(err);
+		console.log('now inside of the function(err)');
+		/*
+		res.send({
+			message: 'now inside of the function(err)'
+		});
+		*/
+		console.log('this is a test');
+		if (err)
+		{
+			console.log('there was an error');
+			/*
+			res.send({
+				message: 'there was an error'
+			});
+			*/
+		}
+		else
+		{
+			console.log('the user has been deleted');
+			/*
+			res.send({
+				message: 'the user has been deleted'
+			});
+			*/
+		}
+
+	});
+	console.log('SUCCESS');
+});
+
+/*
+// OLD CODE
+router.delete('/id', function(req, res, next) {
+	res.send({
+		message: 'we are inside the delete user method'
+	});
 	User.remove({ _id: req.params.id })
 		.then(function() {
 			res.status(204).end();
+			res.send({
+				message: 'the user has been deleted'
+			});
 		}, function (err) {
+			res.send({
+				message: 'there was an error'
+			});
 			if (err) {
 				return next(err);
 			}
 		});
+	res.send({
+		message: 'we are exiting the delete user method'
+	});
 });
+*/
 
 module.exports = router;
