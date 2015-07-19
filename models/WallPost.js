1a37cffa2a135feab5a001bf0276c43d8a750c42
Max Dumas <maltor124@gmail.com>
2015-07-19 16:02:30 -0400

    Establish linter compliance for wallPosts

--- models/WallPost.js
+++ models/WallPost.js
@@ -2,10 +2,10 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var wallPostSchema = new Schema({
-	content: { type: String, required: false },
-	rating: { type: Number, required: true },
-	location: { type: String, required: true },
-	timeOfPost: { type: Date, required: false, default: Date.now() }
+	content: {type: String, required: false},
+	rating: {type: Number, required: true},
+	location: {type: String, required: true},
+	timeOfPost: {type: Date, required: false, default: Date.now()}
 });
 
 var WallPost = mongoose.model('WallPost', wallPostSchema);
