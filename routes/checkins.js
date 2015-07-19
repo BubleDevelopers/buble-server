a70b152fe728f934e6d204e7a022f4284131d7a4
Max Dumas <maltor124@gmail.com>
2015-07-19 15:32:46 -0400

    Correct everything wrong with checkins.js

--- routes/checkins.js
+++ routes/checkins.js
@@ -4,11 +4,10 @@ var express = require('express');
 var router = express.Router();
 var Checkin = require('../models/Checkin').Checkin;
 
-
 // NOT USING THIS RIGHT NOW:
 // UNTESTED
 router.get('/near', function(req, res, next) {
-        Checkin.find({ location: { lat: req.querry.lat, long: req.querry.long } })
+	Checkin.find({location: {lat: req.query.lat, long: req.query.long}})
 		.then(function(checkins) {
 			res.status(200).json(checkins);
 		}, function(err) {
@@ -17,9 +16,9 @@ router.get('/near', function(req, res, next) {
 });
 
 // test works
-// localhost:3001/checkins/place/:placeId?placeId=2
+// localhost:3001/checkins/place/2
 router.get('/place/:placeId', function(req, res, next) {
-		Checkin.find({ location : req.params.placeId })
+	Checkin.find({'location.placeId' : req.params.placeId})
 		.then(function(checkins) {
 			res.status(200).json(checkins);
 		}, function(err) {
@@ -28,11 +27,10 @@ router.get('/place/:placeId', function(req, res, next) {
 });
 
 // test works
-// localhost:3001/checkins/:id?_id=559ee3d5347da39805a40e59
+// localhost:3001/checkins/559ee3d5347da39805a40e59
 router.get('/:id', function(req, res, next) {
-	console.log(req);
-	Checkin.find({ _id: req.params._id })
-	.then(function(checkins) {
+	Checkin.find({_id: req.params.id})
+		.then(function(checkins) {
 			res.status(200).json(checkins);
 		}, function(err) {
 			return next(err);
@@ -51,57 +49,28 @@ router.get('/', function(req, res, next) {
 
 // test works
 router.post('/', function(req, res, next) {
-	
 	var checkin = new Checkin({
 		userId: req.body.userId,
 		location: req.body.location
 	});
-	
-	checkin.save(function(err, resp) {
-		if (err){
-			console.log(err);
-			res.send(
-				err //this line is for testing and can easily be changed later
-			);
-		} else {
-			res.send({
-				message: 'the checkin has been saved'
-			});
-		}
-	});
+
+	checkin.save()
+		.then(function() {
+			res.status(201).end();
+		}, function(err) {
+			return next(err);
+		});
 });
 
 // test works
-// localhost:3001/checkins/:id?_id=559ee3d5347da39805a40e59
+// localhost:3001/checkins/559ee3d5347da39805a40e59
 router.delete('/:id', function(req, res, next) {
-	Checkin.remove({ _id: req.body._id }, function(err) {
-		console.log(err);
-		console.log('now inside of the function(err)');
-		/*
-		res.send({
-			message: 'now inside of the function(err)'
+	Checkin.remove({_id: req.params.id})
+		.then(function() {
+			res.status(200).end();
+		}, function(err) {
+			return next(err);
 		});
-		*/
-		if (err)
-		{
-			console.log('there was an error');
-			/*
-			res.send({
-				message: 'there was an error'
-			});
-			*/
-		}
-		else
-		{
-			console.log('the checkin has been deleted');
-			/*
-			res.send({
-				message: 'the user has been deleted'
-			});
-			*/
-		}
-
-	});
 });
 
 module.exports = router;
