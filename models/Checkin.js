8e4037da388cea988bd9edee4ef7e876558990d6
Max Dumas <maltor124@gmail.com>
2015-07-19 15:29:37 -0400

    Revert Checkin to correct model

--- models/Checkin.js
+++ models/Checkin.js
@@ -1,31 +1,17 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-/*
 var checkinSchema = new Schema({
-<<<<<<< HEAD
-	userId: { type: Number, required: true },
-	location: { type: Number, required: true }
-=======
-	userId: { type: Schema.ObjectId, required: true },
-	location: { 
-		lat: { type: Number, required: true },
-		long: { type: Number, required: true },
-		placeId: { type: String, required: true }
+	userId: {type: Schema.ObjectId, required: true},
+	location: {
+		lat: {type: Number, required: true},
+		long: {type: Number, required: true},
+		placeId: {type: String, required: true}
 	},
-	entered: { type: Date, required: true, default: Date.now() },
-	exited: { type: Date, required: false }
->>>>>>> 704603b6fcb0b578561818357c9838c6e3dba4ca
+	arrived: {type: Date, required: true, default: Date.now()},
+	left: {type: Date, required: false}
 });
-*/
-
-var checkinSchema = new Schema({
-	userId: { type: Number, required: true },
-	location: { type: String, required: true }
-});
-
 
 var Checkin = mongoose.model('Checkin', checkinSchema);
 
 exports.Checkin = Checkin;
-
